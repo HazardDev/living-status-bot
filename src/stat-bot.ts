02ca7f75e66900e652e4480020166a9ddb9e9f35
JewsOfHazard <logan.saso2016@gmail.com>
2017-09-19 14:32:10 -0700

    Formatted for debug

--- src/stat-bot.ts
+++ src/stat-bot.ts
@@ -1,6 +1,7 @@
-import { Client, User } from "discord.io";
+import { Client } from "discord.io";
 import * as fs from "fs";
 import { ICommand } from "./commands/command.interface";
+const config = require("./config.json");
 
 interface IStatBotParams {
     autorun: boolean;
@@ -28,15 +29,24 @@ export class StatBot {
 
         });
 
-        this.client.on("message", (user: User, userID: string, channelID: string, message: string) => {
+        this.client.on("message", (user: string, userID: string, channelID: string, message: string) => {
             console.log("Message received!");
+            console.debug(`User: ${user}                \n
+                           User ID: ${userID}           \n
+                           Channel ID: ${channelID}     \n
+                           Message: ${message}`);
 
             this.commands.map((command: ICommand) => {
                 if (message.startsWith(this.triggerChar) &&
                     message.substring(this.triggerChar.length).startsWith(command.name)) {
 
-                    const response = command.run(user, userID, channelID, message);
-                    this.client.sendMessage({ to: channelID, message: response });
+                    try {
+                        const response = command.run(user, userID, channelID, message);
+                        this.client.sendMessage({ to: channelID, message: response });
+                     } catch (e) {
+                        this.client.sendMessage({ to: config.tracebackChannel, message: e });
+                    }
+
                 }
             });
         });

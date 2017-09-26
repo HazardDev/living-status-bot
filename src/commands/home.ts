95567ad94e917c86ea6388713d4cd2c9b4d2732d
JewsOfHazard <logan.saso2016@gmail.com>
2017-09-25 20:31:18 -0700

    Changed arpscan to run on !home command, not any time else

--- src/commands/home.ts
+++ src/commands/home.ts
@@ -4,25 +4,29 @@ const macAddresses = require('../config.json').macAddresses;
 
 let home = {};
 
-function doScan() {
+function doScan(): Promise<undefined> {
 
-    arpscan({sudo: true})
-        .then((results) => {
-            console.log("Finished arpscan.");
-            results.map((entry) => {
-                console.log(`Processing entry: ${entry.mac}`);
-                if (Object.keys(macAddresses).indexOf(entry.mac.toLowerCase()) !== -1) {
-                    console.log(`Adding entry: ${entry.mac}`);
-                    home[macAddresses[entry.mac.toLowerCase()]] = new Date().getTime();
-                }
+    return new Promise((resolve, reject) => {
+        console.log("Starting arpscan.");
+        arpscan({ sudo: true })
+            .then((results) => {
+                console.log("Finished arpscan.");
+                results.map((entry) => {
+                    console.log(`Processing entry: ${entry.mac}`);
+                    if (Object.keys(macAddresses).indexOf(entry.mac.toLowerCase()) !== -1) {
+                        console.log(`Adding entry: ${entry.mac}`);
+                        home[macAddresses[entry.mac.toLowerCase()]] = new Date().getTime();
+                    }
+                });
+                resolve();
+            })
+            .catch((error) => {
+                console.log(`Arpscan error: ${error}`);
+                reject(error);
             });
-        })
-        .catch((error) => {
-            console.log(`Arpscan error: ${error}`);
-            throw error;
-        });
+    });
 
-    setTimeout(doScan, 1 * 10 * 1000);
+    // setTimeout(doScan, 1 * 10 * 1000);
 
 }
 
@@ -31,22 +35,28 @@ export = {
     name: "home",
 
     run: (user: string, userID: string, channelID: string, message: string): string => {
-        const now: number = new Date().getTime();
-        const atHome: string[] = [];
-        // tslint:disable-next-line:forin
-        for (const person in home) {
-            console.log(`Person entry - ${person} - ${home[person]}`);
-            if ((now - home[person]) < (1000 * 60 * 2)) {
-                console.log(`${person} is home!`);
-                atHome.push(person);
-            }
-        }
 
-        return atHome.length !== 0 ? atHome.join(", ") : "Nobody is home.";
+        doScan().then(() => {
+            const now: number = new Date().getTime();
+            const atHome: string[] = [];
+            // tslint:disable-next-line:forin
+            for (const person in home) {
+                console.log(`Person entry - ${person} - ${home[person]}`);
+                if ((now - home[person]) < (1000 * 60 * 2)) {
+                    console.log(`${person} is home!`);
+                    atHome.push(person);
+                }
+            }
+
+            return atHome.length !== 0 ? atHome.join(", ") : "Nobody is home.";
+        }).catch((error) => {
+            throw error;
+        })
+
     },
 
     start() {
         console.log(`Mac addressed loaded! ${macAddresses}`);
-        doScan();
+        // doScan();
     },
 };
